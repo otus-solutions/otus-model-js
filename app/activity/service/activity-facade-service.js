2e4f58de4c093245e25656058a1511e42586d7a1
emanoelvianna <emanoel.vianna@outlook.com>
2016-07-12 21:15:33 -0300

    FOR-60 - pacial, activity-facade-service

--- app/activity/service/activity-facade-service.js
+++ app/activity/service/activity-facade-service.js
@@ -6,18 +6,61 @@
         .service('ActivityFacadeService', ActivityFacadeService);
 
     ActivityFacadeService.$inject = [
-        'AnswerFactory'
+        'FillingManagerService',
+        'StatusHistoryManagerService',
+        'AnswerFillFactory',
+        'MetadataFillFactory',
+        'QuestionFillFactory',
+        'ActivitySurvey'
     ];
 
-    function ActivityFacadeService(AnswerFactory) {
+    function ActivityFacadeService(FillingManagerService, StatusHistoryManagerService, AnswerFillFactory, MetadataFillFactory, QuestionFillFactory, ActivitySurvey) {
         var self = this;
 
         /* Public interface */
         self.fillQuestion = fillQuestion;
 
-        function fillQuestion(Question, answerValue, Metadata) {
-            var answer = AnswerFactory.create(Question, answerValue, Metadata);
-            //TODO: adicioanr reposta na lista
+        /**
+
+        var reposta = fachada.criarResposta(reposta);
+        var metadata = fachada.criarMetadata(metadata);
+        fachada.criarQuestao(1, reposta, metadata, comentario);
+
+        **/
+
+        function createAnswerFill(answer) {
+            return AnswerFillFactory.create(answer);
+        }
+
+        function createMetadataFill(metadata) {
+            return MetadataFillFactory.create(metadata);
+        }
+
+        function createQuestionFill(questionID, answer, metadata, comment) {
+            var question = QuestionFillFactory.create(questionID, answer, metadata, comment);
+            //TODO: adicionar na lista de fillingList, é preciso decidir quando iniciar a lista
+            FillingManagerService.updateFilling(question);
+            return question;
+        }
+
+        function addFillingOnList(question) {
+            FillingManagerService.updateFilling(question);
+        }
+
+        function updateFillingOnList(question) {
+            FillingManagerService.updateFilling(question);
+        }
+
+        function removeFillingOnList(question) {
+            // FillingManagerService._removeFilling(question.questionID);
+        }
+
+        function searchFillingByIDOnList(questionID) {
+            return FillingManagerService.searchFillingByID(questionID);
+        }
+
+        function createActivitySurvey(category, group, templateOID) {
+            return ActivitySurvey.create(category, group, templateOID, FillingManagerService, StatusHistoryManagerService)
         }
     }
 
