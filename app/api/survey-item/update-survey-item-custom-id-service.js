74d4302679d49e733ce7ff753561b93610fec1ff
fagner <fagnerschwalm@gmail.com>
2016-07-29 11:29:13 -0300

    EXP-58 - Criado serviço para atualizar o customID.

---
+++ app/api/survey-item/update-survey-item-custom-id-service.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('otusjs.surveyItem')
+        .service('UpdateSurveyItemCustomID', UpdateSurveyItemCustomID);
+
+    function UpdateSurveyItemCustomID() {
+        var self = this;
+
+        self.execute = execute;
+
+        function execute(item, id) {
+            // it needs a service to validate if is a valid or available id
+            item.customID = id;
+        }
+    }
+
+}());
