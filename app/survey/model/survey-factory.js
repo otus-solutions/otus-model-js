4be078e9943d19251a0188d0076ba67ab38e0a54
Diego Santos <santos.diegoferreira@gmail.com>
2016-09-09 18:07:16 -0300

    Correção em toJson de Survey.

--- app/survey/model/survey-factory.js
+++ app/survey/model/survey-factory.js
@@ -115,7 +115,11 @@
 
             json.navigationList = [];
             NavigationManagerService.getNavigationList().forEach(function(navigation) {
-                json.navigationList.push(navigation.toJson());
+                if (navigation) {
+                  json.navigationList.push(navigation.toJson());
+                } else {
+                  json.navigationList.push({});                  
+                }
             });
 
             return JSON.stringify(json).replace(/"{/g, '{').replace(/\}"/g, '}').replace(/\\/g, '').replace(/ ":/g, '":');
