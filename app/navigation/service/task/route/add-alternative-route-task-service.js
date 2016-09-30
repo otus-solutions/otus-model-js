561a9cb4807c27b4195e9a976d11da5e49afe2e7
emanoelvianna <emanoel.vianna@outlook.com>
2016-09-30 13:29:32 -0300

    NAV-66 parcial, modificação na forma em que é selecionada a answer

--- app/navigation/service/task/route/add-alternative-route-task-service.js
+++ app/navigation/service/task/route/add-alternative-route-task-service.js
@@ -36,7 +36,7 @@
     function _setupRules(ruleData) {
       var when = ruleData.when.customID || ruleData.when;
       var operator = ruleData.operator.type || ruleData.operator;
-      var answer = (ruleData.answer.option) ? ruleData.answer.option.value : ruleData.answer;
+      var answer = ruleData.getCorrectAnswer();
       return RuleFactory.create(when, operator, answer, ruleData.isMetadata);
     }
 
