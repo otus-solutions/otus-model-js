38a1977b671c52a51741a447b52cf37e41203ffc
brenoscheffer <brenoscheffer@gmail.com>
2017-06-26 16:42:48 -0300

    OMJ-6 Adição de json descritor das aliquotas ao teste da factory - em andamento

--- tests/unit/laboratory/participant/aliquot/participant-aliquot-factory-spec.js
+++ tests/unit/laboratory/participant/aliquot/participant-aliquot-factory-spec.js
@@ -5,6 +5,7 @@ fdescribe('ParticipantAliquotFactory', function() {
   beforeEach(function() {
     module('otusjs.laboratory');
 
+    mockLabParticipant();
     inject(function(_$injector_) {
       var injections = {
         'AliquotCollectionDataFactory': mockAliquoteCollectionDataFactory(_$injector_)
@@ -26,17 +27,17 @@ fdescribe('ParticipantAliquotFactory', function() {
       expect(aliquote.objectType).toEqual('Aliquot');
     });
 
-    it('should generate the same values for this fields', function(){
+    it('should generate the same values for this fields', function() {
       var aliquote = factory.create(aliquoteInfo);
 
       expect(aliquote.objectType).toEqual(aliquoteInfo.objectType);
       expect(aliquote.code).toEqual(aliquoteInfo.code);
       expect(aliquote.name).toEqual(aliquoteInfo.name);
       expect(aliquote.container).toEqual(aliquoteInfo.container);
-      expect(aliquote.role).toEqual(aliquoteInfo.role);      
+      expect(aliquote.role).toEqual(aliquoteInfo.role);
     });
 
-    it('should not call the AliquotCollectionDataFactory when any collectionData is given', function(){
+    it('should not call the AliquotCollectionDataFactory when any collectionData is given', function() {
       var aliquote = factory.create(aliquoteInfo);
       expect(Mock.AliquotCollectionDataFactory.create).not.toHaveBeenCalled();
     });
@@ -44,12 +45,12 @@ fdescribe('ParticipantAliquotFactory', function() {
 
   });
 
-  describe('the fromJSON method', function(){
+  describe('the fromJSON method', function() {
     it('should call AliquotCollectionDataFactory.create', function() {
-        aliquoteInfo.collectionData = {
-          time: '2017-06-26T05:50:19.434Z',
-          operator: 'lalala@gmail.com'
-        };
+      aliquoteInfo.collectionData = {
+        time: '2017-06-26T05:50:19.434Z',
+        operator: 'lalala@gmail.com'
+      };
       var aliquote = factory.create(aliquoteInfo);
       expect(Mock.AliquotCollectionDataFactory.create).toHaveBeenCalledWith(aliquoteInfo.collectionData);
     });
@@ -60,6 +61,100 @@ fdescribe('ParticipantAliquotFactory', function() {
     Mock.AliquotCollectionDataFactory = _$injector_.get('otusjs.laboratory.AliquotCollectionDataFactory');
     spyOn(Mock.AliquotCollectionDataFactory, 'create').and.callThrough();
     return Mock.AliquotCollectionDataFactory;
+  }
+
+  function mockLabParticipant() {
+    Mock.AliquotConfiguration = {
+      objectType: "AliquotConfiguration",
+      aliquotMomentDescriptors: [{
+          objectType: "AliquotMoment",
+          name: "FASTING",
+          aliquotTypesDescriptors: [{
+             objectType: "AliquotType",
+             name: "EDTA",
+             aliquots: [
+             {
+                objectType: "AliquoteDescriptor",
+                name: "STORAGE",
+                label: "Armazenamento",
+                quantity: 5
+             }, {
+                objectType: "AliquoteDescriptor",
+                name: "BIOSORO_AL",
+                label: "Biosoro",
+                quantity: 2
+             }, {
+                objectType: "AliquoteDescriptor",
+                name: "",
+                label: "",
+                quantity: 0
+             }
+             ]
+          },
+          {
+             objectType: "AliquotType",
+             name: "GEL",
+             aliquots: [
+                {
+                   objectType: "AliquoteDescriptor",
+                   name: "BIOSORO",
+                   label: "Bioquímica Soro",
+                   quantity: 1
+                },
+                {
+                   objectType: "AliquoteDescriptor",
+                   name: "PCR",
+                   label: "PCR",
+                   quantity: 1
+                },
+                {
+                   objectType: "AliquoteDescriptor",
+                   name: "FASTING_INSULINE",
+                   label: "Insulina Jejum",
+                   quantity: 1
+                },
+                {
+                   objectType: "AliquoteDescriptor",
+                   name: "BIOSORO_CQ",
+                   label: "Bioquímica Soro CQ",
+                   quantity: 2
+                },
+                {
+                   objectType: "AliquoteDescriptor",
+                   name: "FASTING_INSULINE_CQ",
+                   label: "Insulina Jejum CQ",
+                   quantity: 2
+                },
+                {
+                   objectType: "AliquoteDescriptor",
+                   name: "STORAGE",
+                   label: "Armazenamento",
+                   quantity: 8
+                },
+                {
+                   objectType: "AliquoteDescriptor",
+                   name: "STORAGE_CQ",
+                   label: "Armazenamento",
+                   quantity: 8
+                }
+             ]
+          }]
+      }, {
+          objectType: "AliquotMoment",
+          name: "POST_OVERLOAD",
+          aliquotTypesDescriptors: []
+      }],
+      containerDescriptor: [{
+          objectType: "ContainerDescriptor",
+          name: "PALLET",
+          label: "Palheta"
+      }, {
+          objectType: "ContainerDescriptor",
+          name: "CRIOTUBE",
+          label: "Criotubo"
+      }]
+   };
+
   }
 
 });
