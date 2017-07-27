4d8a77ab71c81b77ab7d91102d637eeb4a3d5254
brenoscheffer <brenoscheffer@gmail.com>
2017-07-27 19:59:42 -0300

    OMJ-9 httpBackend adicionado ao teste de serviço de transporte

--- tests/unit/laboratory/transportation/transportation-service-spec.js
+++ tests/unit/laboratory/transportation/transportation-service-spec.js
@@ -1,24 +1,34 @@
 fdescribe('the transportation service', function() {
    var Mock = {};
    var service;
+   var $httpBackend;
 
    beforeEach(function() {
      module('otusjs.laboratory.transportation');
+
 
      inject(function(_$injector_) {
        var injections = {
          'LotFactory': mockTransportationLotFactory(_$injector_),
          '$http':  _$injector_.get('$http')
        };
+       $httpBackend = _$injector_.get('$httpBackend');
+       $httpBackend.when('GET', 'app/laboratory/transportation/aliquot-list.json')
+                            .respond({userId: 'userX'}, {'A-Token': 'xxx'});
        service = _$injector_.get('otusjs.laboratory.transportation.TransportationService', injections);
      });
    });
 
    it('should', function () {
+      service.get();
+      $httpBackend.flush();
 
    });
 
    function mockTransportationLotFactory(_$injector_) {
-      Mock.LotFactory = _$injector_.get('otusjs.laboratory.transportation.LotFactory');
+      var injections = {
+        'TransportationAliquotFactory': _$injector_.get('otusjs.laboratory.transportation.TransportationAliquotFactory')
+      };
+      Mock.LotFactory = _$injector_.get('otusjs.laboratory.transportation.LotFactory', injections);
    }
 });
