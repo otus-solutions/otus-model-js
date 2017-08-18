f37b1bbfa470e37a9dc97fd336a865f6fdcf071d
Allister T. Ramos <allistertr@hotmail.com>
2017-08-18 16:57:12 -0300

    BUG-DAY-1 - Meus testes

---
+++ tests/unit/laboratory/participant/aliquot/aliquot-collection-data-factory-spec.js
@@ -0,0 +1,61 @@
+fdescribe('aliquot collection data factory', function() {
+  var Mock = {};
+
+  beforeEach(function() {
+    module('otusjs.laboratory');
+
+    inject(function(_$injector_) {
+      factory = _$injector_.get('otusjs.laboratory.AliquotCollectionDataFactory');
+    });
+
+    //...............
+    mockCollectionInfo();
+    mockInstanceOfAliquotCollectionDataFull();
+    mockInstanceOfAliquotCollectionDataEmpty();
+    //...............
+  });
+
+
+  //...............
+  fit('should create an AliquotCollectionData object', function() {
+    var object = factory.create(Mock.CollectionInfo);
+    expect(object.objectType).toEqual("AliquotCollectionData");
+  });
+  
+  fit('should stringfy an AliquotCollectionData', function() {
+    var object = factory.create(Mock.CollectionInfo);
+    expect(JSON.stringify(object)).toEqual(JSON.stringify(Mock.InstanceOfAliquotCollectionDataFull));
+  });
+  //...............
+
+
+  //..........................
+  function mockCollectionInfo() {
+    Mock.CollectionInfo = {
+      metadata: 'SKIP',
+      operator: 'jose@jose.eu',
+      time: 'mytime'
+    };
+  }
+
+  function mockInstanceOfAliquotCollectionDataFull() {
+    Mock.InstanceOfAliquotCollectionDataFull = {
+      objectType: 'AliquotCollectionData',
+      metadata: 'SKIP',
+      operator: 'jose@jose.eu',
+      time: 'mytime'
+    };
+  }
+
+  function mockInstanceOfAliquotCollectionDataEmpty() {
+    Mock.InstanceOfAliquotCollectionDataEmpty = {
+      objectType: 'AliquotCollectionData',
+      metadata: '',
+      operator: '',
+      time: ''
+    };
+  }
+  //..........................
+
+
+});
