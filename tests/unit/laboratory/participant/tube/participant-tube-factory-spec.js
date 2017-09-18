2c4378f643aec28eb7a670a731ff8986d9aea154
Tiago Matana <tiago.matana@gmail.com>
2017-09-18 15:47:09 -0300

    OMJ-19 Participant-tube-factory 2 Tests

--- tests/unit/laboratory/participant/tube/participant-tube-factory-spec.js
+++ tests/unit/laboratory/participant/tube/participant-tube-factory-spec.js
@@ -1,3 +1,88 @@
 describe('the participantTubeFactory', function() {
-  
+  var Mock = {};
+  var factory;
+
+  beforeEach(function() {
+    angular.mock.module('otusjs.laboratory');
+
+    inject(function(_$injector_) {
+      var injections = {
+        'TubeCollectionDataFactory': mockTubeCollectionDataFactory(
+          _$injector_),
+        'ParticipantAliquotFactory': mockParticipantAliquotFactory(
+          _$injector_),
+        'LaboratoryConfigurationService': mockLaboratoryConfigurationService(
+          _$injector_)
+      };
+      factory = _$injector_.get(
+        'otusjs.laboratory.participant.ParticipanTubeFactory',
+        injections);
+
+    });
+
+    mockSelectedParticipant();
+    mockParticipantLaboratory();
+    mockLabDescriptors();
+    mockSingleTube();
+    mockLoggedUser();
+
+    Mock.LaboratoryConfigurationService.initializeParticipantConfiguration(
+      Mock.SelectedParticipant, "DEFAULT");
+    Mock.LaboratoryConfigurationService.initializeLaboratoryConfiguration(
+      Mock.LabDescriptors);
+
+  });
+
+  it('should create an ParticipanTubeFactory object ', function() {
+    var object = factory.create(Mock.singleTube, Mock.LoggedUser);
+    expect(object.objectType).toEqual("Tube");
+
+  });
+
+  it('should build a tube from Array', function() {
+    var object = factory.buildFromArray([Mock.singleTube], Mock.LoggedUser);
+    expect(object[0].objectType).toEqual("Tube");
+  });
+
+  function mockTubeCollectionDataFactory(_$injector_) {
+    Mock.TubeCollectionDataFactory = _$injector_.get(
+      'otusjs.laboratory.participant.TubeCollectionDataFactory');
+    spyOn(Mock.TubeCollectionDataFactory, 'create').and.callThrough();
+    // spyOn(Mock.TubeCollectionDataFactory, 'TubeCollectionData').and.callThrough(); //private method
+    return Mock.TubeCollectionData;
+  }
+
+  function mockParticipantAliquotFactory(_$injector_) {
+    Mock.ParticipantAliquotFactory = _$injector_.get(
+      'otusjs.laboratory.participant.ParticipantAliquotFactory');
+    spyOn(Mock.ParticipantAliquotFactory, 'create').and.callThrough();
+    return Mock.ParticipantAliquotFactory;
+
+  }
+
+  function mockLaboratoryConfigurationService(_$injector_) {
+    Mock.LaboratoryConfigurationService = _$injector_.get(
+      'otusjs.laboratory.configuration.LaboratoryConfigurationService');
+    return Mock.LaboratoryConfigurationService;
+  }
+
+  function mockSelectedParticipant() {
+    Mock.SelectedParticipant = angular.copy(Test.utils.data.selectedParticipant); //json-importer.js
+  }
+
+  function mockParticipantLaboratory() {
+    Mock.ParticipantLaboratory = Test.utils.data.participantLaboratory; //json-importer.js
+  }
+
+  function mockLabDescriptors() {
+    Mock.LabDescriptors = Test.utils.data.laboratoryConfiguration; //json-importer.js
+  }
+
+  function mockSingleTube() {
+    Mock.singleTube = Mock.ParticipantLaboratory.tubes[0];
+  }
+
+  function mockLoggedUser() {
+    Mock.LoggedUser = Test.utils.data.otusLoggedUser;
+  }
 });
