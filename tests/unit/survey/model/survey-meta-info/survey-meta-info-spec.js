f592977d40623b6306170c33b929929196f93640
Adriano Boese <desenvacc@gmail.com>
2017-09-22 14:33:58 -0300

    OMJ-19 survey-meta-info Tests

--- tests/unit/survey/model/survey-meta-info/survey-meta-info-spec.js
+++ tests/unit/survey/model/survey-meta-info/survey-meta-info-spec.js
@@ -1,4 +1,4 @@
-xdescribe('SurveyMetaInfo', function() {
+describe('SurveyMetaInfo', function() {
     var Mock = {};
     var surveyMetainfo;
     var factory;
@@ -6,35 +6,62 @@ xdescribe('SurveyMetaInfo', function() {
     beforeEach(function() {
         angular.mock.module('otusjs');
 
-        mockDatetime();
-        mockJson();
-
+        mockJsonObject();
         inject(function(_$injector_) {
             factory = _$injector_.get('SurveyMetaInfoFactory');
+
             surveyMetainfo = factory.create();
         });
     });
 
     describe('toJson method', function() {
-
         it('should return a well formatted json based on instance of SurveyMetaInfo', function() {
-            expect(surveyMetainfo.toJson()).toEqual(Mock.json);
+          Mock.jsonObject.creationDatetime = new Date();
+          expect(surveyMetainfo.toJson()).toEqual(JSON.stringify(Mock.jsonObject));
         });
-
     });
 
-    function mockJson() {
-        Mock.json = JSON.stringify({
-            extents: 'StudioObject',
-            objectType: 'SurveyMetaInfo',
-            creationDatetime: Mock.now,
-            otusStudioVersion: ''
+    describe('fromJsonObject method', function() {
+
+        beforeEach(function () {
+          mockSurveyMetaInfoFromJson(Mock.jsonObject);
         });
+
+        it('should create a SurveyMetaInfo type object with extents equal to StudioObject', function() {
+          expect(Mock.surveyMetaInfoFromJson.extents).toEqual('StudioObject');
+        });
+
+        it('should create a SurveyMetaInfo type object with objectType equal to SurveyMetaInfo', function() {
+          expect(Mock.surveyMetaInfoFromJson.objectType).toEqual('SurveyMetaInfo');
+        });
+
+        it('should create a SurveyMetaInfo type object with creationDatetime equal to 2017-09-22T16:50:28.708Z', function() {
+          expect(Mock.surveyMetaInfoFromJson.creationDatetime).toEqual('2017-09-22T16:50:28.708Z');
+        });
+
+        it('should create a SurveyMetaInfo type object with creationDatetime equal to 2017-09-22T16:50:28.708Z', function() {
+
+          var ERROR_MESSAGE = "otusjs.model.survey.model.SurveyMetaInfoFactory.fromJsonObject() method expects to receive a object instead a String";
+
+          var fromJsonObjectFunction = function() {
+            factory.fromJsonObject(JSON.stringify(Mock.jsonObject));
+          };
+
+          expect(fromJsonObjectFunction).toThrowError(ERROR_MESSAGE);
+        });
+    });
+
+    function mockJsonObject() {
+      Mock.jsonObject = {
+        extents: 'StudioObject',
+        objectType: 'SurveyMetaInfo',
+        creationDatetime: "2017-09-22T16:50:28.708Z",
+        otusStudioVersion: ''
+      }
     }
 
-    function mockDatetime() {
-        Mock.now = Date.now();
-        spyOn(Date, 'now').and.returnValue(Mock.now);
+    function mockSurveyMetaInfoFromJson(jsonObject) {
+       Mock.surveyMetaInfoFromJson = factory.fromJsonObject(jsonObject);
     }
 
 });
