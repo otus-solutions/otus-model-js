b6bb7adda7b674597f43e1062a474d3b94753fc1
Tiago Matana <tiago.matana@gmail.com>
2018-05-08 17:03:23 -0300

    OMJ-31 Tests unitários

--- tests/unit/survey/model/survey/survey-form-factory-spec.js
+++ tests/unit/survey/model/survey/survey-form-factory-spec.js
@@ -2,17 +2,12 @@ describe('SurveyFormFactory', function() {
   var Mock = {};
   var factory;
   var surveyForm;
-  var options = {
-    sender: "tiago.matana@gmail.com",
-    sendingDate : "2017-09-28T23:25:31.274Z",
-    surveyFormType : "FORM_INTERVIEW",
-    surveyTemplate : {}
-  };
+  var survey;
 
   beforeEach(function() {
     angular.mock.module('otusjs');
 
-    Mock.surveyTemplate = Test.utils.data.latestSurveyTemplate;
+    Mock.surveyTemplate = Test.utils.data.survey;
 
     inject(function(_$injector_) {
       factory = _$injector_.get('SurveyFormFactory', {
@@ -24,11 +19,41 @@ describe('SurveyFormFactory', function() {
   describe('SurveyFormFactory.create()', function() {
 
     beforeEach(function() {
-      surveyForm = factory.create(options);
+      surveyForm = factory.create(jasmine.any(Object));
     });
 
     it('should return a SurveyForm that extends from StudioObject', function() {
       expect(surveyForm.extents).toEqual('StudioObject');
+    });
+    xit('should return a SurveyForm that version value', function() {
+      expect(typeof surveyForm.version).toEqual("number");
+      expect(surveyForm.version).toEqual(null);
+    });
+    it('should return a SurveyForm that isDiscarded value', function() {
+      expect(surveyForm.isDiscarded).toEqual(false);
+    });
+
+    it('should return a SurveyForm object type', function() {
+      expect(surveyForm.objectType).toEqual('SurveyForm');
+    });
+
+  });
+
+  describe('SurveyFormFactory.fromJsonObject()', function() {
+
+    beforeEach(function() {
+      surveyForm = factory.fromJsonObject(Mock.surveyTemplate);
+    });
+
+    it('should return a SurveyForm that extends from StudioObject', function() {
+      expect(surveyForm.extents).toEqual('StudioObject');
+    });
+    it('should return a SurveyForm that version value', function() {
+      expect(typeof surveyForm.version).toEqual("number");
+      expect(surveyForm.version).toEqual(1);
+    });
+    it('should return a SurveyForm that isDiscarded value', function() {
+      expect(surveyForm.isDiscarded).toEqual(true);
     });
 
     it('should return a SurveyForm object type', function() {
