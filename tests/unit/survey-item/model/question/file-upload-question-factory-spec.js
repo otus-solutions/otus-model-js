915b493a0dea41bacf35766c7f1575a171d56716
Adonis Garcia <adonis.garcia.adg@gmail.com>
2018-09-11 10:54:21 -0300

    OMJ-34 - Create of unit test

--- tests/unit/survey-item/model/question/file-upload-question-factory-spec.js
+++ tests/unit/survey-item/model/question/file-upload-question-factory-spec.js
@@ -1,11 +1,13 @@
 describe("FileUploadQuestionFactory", function() {
   var Mock = {};
   var factory;
+  var survey;
 
   /* @BeforeScenario */
   beforeEach(function() {
     angular.mock.module('otusjs');
     mockParams();
+    mockJsonObject();
 
     inject(function(_$injector_) {
       var injections = {
@@ -35,6 +37,29 @@ describe("FileUploadQuestionFactory", function() {
       null);
   });
 
+  describe('should create an instance with the same values of toJson', function() {
+
+    beforeEach(function() {
+      survey = factory.fromJsonObject(Mock.jsonObject);
+    });
+
+    it('should return a FileUploadQuestionFactory that extents', function() {
+      expect(survey.extents).toEqual('SurveyItem');
+    });
+
+    it('should return a FileUploadQuestionFactory that objectType', function() {
+      expect(survey.objectType).toEqual('FileUploadQuestion');
+    });
+
+    it('should return a FileUploadQuestionFactory that dataType', function() {
+      expect(survey.dataType).toEqual('Binary');
+    });
+
+    it('should create an instance with the same values of Mock.jsonObject', function() {
+      expect(survey.toJson()).toEqual(JSON.stringify(Mock.jsonObject));
+    });
+
+  });
 
 
   function mockLabelFactory(_$injector_) {
@@ -59,4 +84,58 @@ describe("FileUploadQuestionFactory", function() {
     };
     Mock.templateID = "TST1";
   }
+
+  function mockJsonObject() {
+    Mock.jsonObject = {
+      "extents": "SurveyItem",
+      "objectType": "FileUploadQuestion",
+      "templateID": "DQUOTETWO1",
+      "customID": "DQUOTETWO1",
+      "dataType": "Binary",
+      "label": {
+        "ptBR": {
+          "extends": "StudioObject",
+          "objectType": "Label",
+          "oid": "",
+          "plainText": "\"aspas\"",
+          "formattedText": "\"aspas\""
+        },
+        "enUS": {
+          "extends": "StudioObject",
+          "objectType": "Label",
+          "oid": "",
+          "plainText": "",
+          "formattedText": ""
+        },
+        "esES": {
+          "extends": "StudioObject",
+          "objectType": "Label",
+          "oid": "",
+          "plainText": "",
+          "formattedText": ""
+        }
+      },
+      "metadata": {
+        "extents": "StudioObject",
+        "objectType": "MetadataGroup",
+        "options": []
+      },
+      "fillingRules": {
+        "extends": "StudioObject",
+        "objectType": "FillingRules",
+        "options": {
+          "mandatory": {
+            "extends": "StudioObject",
+            "objectType": "Rule",
+            "validatorType": "mandatory",
+            "data": {
+              "canBeIgnored": false,
+              "reference": true
+            }
+          }
+        }
+      }
+    };
+  }
+
 });
