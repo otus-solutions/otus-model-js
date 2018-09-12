69520bb779553f70b9b4e294c395741bb21f914e
fdrtec <fdrtec@gmail.com>
2018-09-12 14:05:16 -0300

    OMJ-34 unitTest ruleFactory

--- tests/unit/navigation/model/rule/rule-factory-new-spec.js
+++ tests/unit/navigation/model/rule/rule-factory-new-spec.js
@@ -6,7 +6,10 @@
         var OPERATOR = "otus";
         var ANSWER = 1;
         var IS_METADATA = false;
-        var IS_CUSTOM = false;        
+        var IS_CUSTOM = false;
+        var PARAMETER_TOOL = "for confirmation of use and comparison";
+        var POSITIVE_BOOLEAN = true;        
+        var NEGATIVE_BOOLEAN = false;        
 
     
         beforeEach(function () {
@@ -36,45 +39,45 @@
             });
             
             it('withinMethod should to modify operator and answer attributes', function(){                
-                rule.within(['item']);
+                rule.within([PARAMETER_TOOL]);
                 expect(rule.operator).toBe("within");
-                expect(rule.answer[0]).toBe("item");
+                expect(rule.answer[0]).toBe(PARAMETER_TOOL);
             });
 
             it('notEqualMethod should to modify operator and answer attributes', function(){                
-                rule.notEqual(Boolean.False);
+                rule.notEqual(PARAMETER_TOOL);
                 expect(rule.operator).toBe("notEqual");
-                expect(rule.answer).toBe(Boolean.False);
+                expect(rule.answer).toBe(PARAMETER_TOOL);
             });
 
             it('equalMethod should to modify operator and answer attributes', function(){                
-                rule.equal(Boolean.True);
+                rule.equal(PARAMETER_TOOL);
                 expect(rule.operator).toBe("equal");
-                expect(rule.answer).toBe(Boolean.True);
+                expect(rule.answer).toBe(PARAMETER_TOOL);
             });
 
             it('greatMethod should to modify operator and answer attributes', function(){                
-                rule.greater(Boolean.True);
+                rule.greater(PARAMETER_TOOL);
                 expect(rule.operator).toBe('greater');
-                expect(rule.answer).toBe(Boolean.True);
+                expect(rule.answer).toBe(PARAMETER_TOOL);
             });
 
             it('greaterEqualMethod should to modify operator and answer attributes', function(){                
-                rule.greaterEqual(Boolean.True);
+                rule.greaterEqual(PARAMETER_TOOL);
                 expect(rule.operator).toBe('greaterEqual');
-                expect(rule.answer).toBe(Boolean.True);
+                expect(rule.answer).toBe(PARAMETER_TOOL);
             });
 
             it('lowerMethod should to modify operator and answer attributes', function(){                
-                rule.lower(Boolean.True);
+                rule.lower(PARAMETER_TOOL);
                 expect(rule.operator).toBe('lower');
-                expect(rule.answer).toBe(Boolean.True);
+                expect(rule.answer).toBe(PARAMETER_TOOL);
             });
 
             it('lowerEqualMethod should to modify operator and answer attributes', function(){                
-                rule.lowerEqual(Boolean.True);
+                rule.lowerEqual(PARAMETER_TOOL);
                 expect(rule.operator).toBe('lowerEqual');
-                expect(rule.answer).toBe(Boolean.True);
+                expect(rule.answer).toBe(PARAMETER_TOOL);
             });
 
             it('betweenMethod should to modify operator and answer attributes if first argument is array', function(){                
@@ -90,15 +93,52 @@
             });
 
             it('containsMethod should to modify operator and answer attributes if first argument is not array', function(){                
-                rule.contains('context');
+                rule.contains(PARAMETER_TOOL);
                 expect(rule.operator).toBe('contains');
-                expect(rule.answer).toBe('context');
+                expect(rule.answer).toBe(PARAMETER_TOOL);
             });
 
-            it('equalsMethod should to modify operator and answer attributes if first argument is not array', function(){                
-                expect(rule.equals(other)).toBe(true);
-                
+            it('equalsMethod should return positive if objectTypeValues are equal', function(){                
+                expect(rule.equals(other)).toBeTruthy();                
             });
+
+            it('equalsMethod should return negative if objectTypeValues are not equal', function(){                
+                other.objectType = PARAMETER_TOOL;
+                expect(rule.equals(other)).toBeFalsy();                
+            });
+
+            it('equalsMethod should return negative if whenValues are not equal', function(){                
+                other.when = PARAMETER_TOOL;
+                expect(rule.equals(other)).toBeFalsy();                
+            });
+
+            it('equalsMethod should return negative if operatorValues are not equal', function(){                
+                other.operator = PARAMETER_TOOL;
+                expect(rule.equals(other)).toBeFalsy();                
+            });
+
+            it('equalsMethod should return negative if answerValues are not equal', function(){                
+                other.answer = PARAMETER_TOOL;
+                expect(rule.equals(other)).toBeFalsy();                
+            });
+
+            it('equalsMethod should return negative if isCustomValues are not equal', function(){                
+                other.isCustom = POSITIVE_BOOLEAN;
+                expect(rule.equals(other)).toBeFalsy();                
+            });
+
+            it('equalsMethod should return negative if isMetadata are not equal', function(){                
+                other.isMetadata = POSITIVE_BOOLEAN;
+                expect(rule.equals(other)).toBeFalsy();                
+            });
+
+            it('cloneMethod should return clone of Rule instance', function(){                
+                var clone = rule.clone();
+                // console.log(typeof clone);
+                //console.log(instanceof rule);
+                // expect(clone == rule).toBeTruthy();
+            });
+
 
 
 
