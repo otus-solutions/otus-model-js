c553b9eb014f2fb0a273f3c5cae79fc5931c3145
emanoelvianna <emanoel.vianna@outlook.com>
2019-04-10 17:56:28 -0300

    OMJ-44 - teste de unidade em desenvolvimento sobre o serviço NavigationInsertionTask

--- tests/unit/navigation/service/task/navigation/navigation-insertion-service-task-spec.js
+++ tests/unit/navigation/service/task/navigation/navigation-insertion-service-task-spec.js
@@ -1,4 +1,4 @@
-describe("NavigationInsertionTask service", function () {
+xdescribe("NavigationInsertionTask service", function () {
   let Mock = {};
   let service;
 
@@ -6,23 +6,53 @@ describe("NavigationInsertionTask service", function () {
     angular.mock.module('otusjs');
 
     inject(function (_$injector_) {
-      _mock(_$injector_);
-
       service = _$injector_.get('otusjs.model.navigation.NavigationInsertionTask', {
         'otusjs.model.navigation.DefaultRouteCreationTaskService': Mock.DefaultRouteCreationTaskService,
         'otusjs.model.navigation.RouteRemovalTaskService': Mock.RouteRemovalTaskService
       });
+      _mock(_$injector_);
+      _mockNavigationContainerService(_$injector_);
     });
-
   });
 
-  it('should create accordingly to objectType', function () {
+  describe('setContainer method', function () {
+    it('when setContainer method is called then container variable should to be not undefined ', function () {
 
+    });
+  });
+
+  describe('execute method', function () {
+    it('should call method execute in DefaultRouteCreationTaskService', function () {
+
+    });
   });
 
   function _mock(_$injector_) {
     Mock.DefaultRouteCreationTaskService = _$injector_.get('otusjs.model.navigation.DefaultRouteCreationTaskService');
     Mock.RouteRemovalTaskService = _$injector_.get('otusjs.model.navigation.RouteRemovalTaskService');
+
+    Mock.navigationToMove = {
+      origin: 'Q1',
+      clearNavigation: function () { }
+    };
+  }
+
+  function _mockNavigationContainerService($injector) {
+    mockQuestions($injector);
+
+    Mock.NavigationContainerService = $injector.get('otusjs.model.navigation.NavigationContainerFactory');
+    Mock.NavigationContainerService.createNavigationTo(Mock.questionOne.templateID, Mock.questionTwo.templateID);
+    Mock.NavigationContainerService.createNavigationTo(Mock.questionTwo.templateID, Mock.questionThree.templateID);
+
+    spyOn(Mock.NavigationContainerService, 'getNavigationByPosition').and.callThrough();
+
+    return Mock.NavigationContainerService;
+  }
+
+  function mockQuestions($injector) {
+    Mock.questionOne = $injector.get('SurveyItemFactory').create('IntegerQuestion', 'Q1');
+    Mock.questionTwo = $injector.get('SurveyItemFactory').create('IntegerQuestion', 'Q2');
+    Mock.questionThree = $injector.get('SurveyItemFactory').create('IntegerQuestion', 'Q3');
   }
 
 });
