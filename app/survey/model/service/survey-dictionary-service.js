97f9e84a122e181fc064bd28afff77c4863d8f30
Fabiano Ramires <fdrtec@gmail.com>
2019-06-27 16:32:28 -0300

    OMJ-55 refatoração dos validations e extraction values para template string

--- app/survey/model/service/survey-dictionary-service.js
+++ app/survey/model/service/survey-dictionary-service.js
@@ -11,20 +11,15 @@
     self.customQuestionTreatment = customQuestionTreatment;
 
     function itemContainerCaptureValues(item){
-      //let values = new Map();
       let values = [];
       if (Array.isArray(item.options)) {
         item.options.map(option => {
           switch (option.objectType) {
             case "CheckboxAnswerOption":
-              //values.set(`[${option.optionID}]:${option.label.ptBR.formattedText}`, option.value );
-              //values.push({[`${option.optionID}.${option.label.ptBR.formattedText}`]: option.value});
-              values.push(`${option.optionID}-${option.value}-${option.label.ptBR.formattedText}`);
+              values.push(`${option.optionID}(${option.value}):${option.label.ptBR.formattedText}`);
               break;
             case "AnswerOption":
-              //values.set(option.label.ptBR.formattedText, option.extractionValue);
-              //values.push({[option.label.ptBR.formattedText]: option.extractionValue});
-              values.push(`${option.extractionValue}-${option.label.ptBR.formattedText}` );
+              values.push(`(${option.extractionValue})${option.label.ptBR.formattedText}`);
               break;
           }
         });
@@ -32,9 +27,7 @@
       if(Array.isArray(item.lines)){
         item.lines.map(line => {
           line.gridTextList.map(gridText => {
-            //values.set(gridText.customID, gridText.unit.ptBR.formattedText);
-            // values.push({[gridText.customID] : gridText.unit.ptBR.formattedText});
-            values.push(`${gridText.customID}-${gridText.unit.ptBR.formattedText}`);
+            values.push(`${gridText.customID}(${gridText.unit.ptBR.formattedText})`);
           });
         });
       }
@@ -51,8 +44,6 @@
         json.label = item.value.ptBR.formattedText
       }
     }
-
-
   }
 
   function _itemContainerCaptureMetadata(item) {
@@ -64,90 +55,151 @@
   }
 
   function _itemContainerCaptureValidatorTypes(item) {
-    let validators = new Map();
+    let validators = [];
 
     if (item.fillingRules.options.mandatory) {
-      validators.set(item.fillingRules.options.mandatory.validatorType,
-        item.fillingRules.options.mandatory.data.reference);
+      validators.push(`${item.fillingRules.options.mandatory.validatorType}(${item.fillingRules.options.mandatory.data.reference})`);
     }
     if (item.fillingRules.options.accept) {
-      validators.set(item.fillingRules.options.accept.validatorType,
-        item.fillingRules.options.accept.data.reference);
+      validators.push(`${item.fillingRules.options.accept.validatorType}(${item.fillingRules.options.accept.data.reference})`);
     }
     if (item.fillingRules.options.distinct) {
-      validators.set(item.fillingRules.options.distinct.validatorType,
-        item.fillingRules.options.distinct.data.reference);
+      validators.push(`${item.fillingRules.options.distinct.validatorType}(${item.fillingRules.options.distinct.data.reference})`);
     }
     if (item.fillingRules.options.lowerLimit) {
-      validators.set(item.fillingRules.options.lowerLimit.validatorType,
-        item.fillingRules.options.lowerLimit.data.reference);
+      validators.push(`${item.fillingRules.options.lowerLimit.validatorType}(${item.fillingRules.options.lowerLimit.data.reference})`);
     }
     if (item.fillingRules.options.upperLimit) {
-      validators.set(item.fillingRules.options.upperLimit.validatorType,
-        item.fillingRules.options.upperLimit.data.reference);
+      validators.push(`${item.fillingRules.options.upperLimit.validatorType}(${item.fillingRules.options.upperLimit.data.reference})`);
     }
     if (item.fillingRules.options.precision) {
-      validators.set(item.fillingRules.options.precision.validatorType,
-        item.fillingRules.options.precision.data.reference);
+      validators.push(`${item.fillingRules.options.precision.validatorType}(${item.fillingRules.options.precision.data.reference})`);
     }
     if (item.fillingRules.options.scale) {
-      validators.set(item.fillingRules.options.scale.validatorType,
-        item.fillingRules.options.scale.data.reference);
+      validators.push(`${item.fillingRules.options.scale.validatorType}(${item.fillingRules.options.scale.data.reference})`);
     }
     if (item.fillingRules.options.in) {
-      validators.set(item.fillingRules.options.in.validatorType,
-        [item.fillingRules.options.in.data.reference.initial + '~' +
-        item.fillingRules.options.in.data.reference.end]);
+      validators.push(`${item.fillingRules.options.in.validatorType}(${[item.fillingRules.options.in.data.reference.initial + '~' +
+        item.fillingRules.options.in.data.reference.end]})`);
     }
     if (item.fillingRules.options.minDate) {
-      validators.set(item.fillingRules.options.minDate.validatorType,
-        item.fillingRules.options.minDate.data.reference.value,
-      );
+      validators.push(`${item.fillingRules.options.minDate.validatorType}(${item.fillingRules.options.minDate.data.reference.value})`);
     }
     if (item.fillingRules.options.maxDate) {
-      validators.set(item.fillingRules.options.maxDate.validatorType,
-        item.fillingRules.options.maxDate.data.reference.value,
-      );
+      validators.push(`${item.fillingRules.options.maxDate.validatorType}(${item.fillingRules.options.maxDate.data.reference.value})`);
     }
     if (item.fillingRules.options.futureDate) {
-      validators.set(item.fillingRules.options.futureDate.validatorType,
-        item.fillingRules.options.futureDate.data.reference,
-      );
+      validators.push(`${item.fillingRules.options.futureDate.validatorType}(${item.fillingRules.options.futureDate.data.reference})`);
     }
     if (item.fillingRules.options.pastDate) {
-      validators.set(item.fillingRules.options.pastDate.validatorType,
-        item.fillingRules.options.pastDate.data.reference,
-      );
+      validators.push(`${item.fillingRules.options.pastDate.validatorType}(${item.fillingRules.options.pastDate.data.reference})`);
     }
     if (item.fillingRules.options.rangeDate) {
-      validators.set(item.fillingRules.options.rangeDate.validatorType,
-        [item.fillingRules.options.rangeDate.data.reference.initial.value + '~' +
-        item.fillingRules.options.rangeDate.data.reference.end.value]
-      );
+      validators.push(`${item.fillingRules.options.rangeDate.validatorType}(${[item.fillingRules.options.rangeDate.data.reference.initial.value + '~' +
+        item.fillingRules.options.rangeDate.data.reference.end.value]})`);
     }
     if (item.fillingRules.options.minSelected) {
-      validators.set(item.fillingRules.options.minSelected.validatorType,
-        item.fillingRules.options.minSelected.data.reference);
+      validators.push(`${item.fillingRules.options.minSelected.validatorType}(${item.fillingRules.options.minSelected.data.reference})`);
     }
     if (item.fillingRules.options.maxSelected) {
-      validators.set(item.fillingRules.options.maxSelected.validatorType,
-        item.fillingRules.options.maxSelected.data.reference);
+      validators.push(`${item.fillingRules.options.maxSelected.validatorType}(${item.fillingRules.options.maxSelected.data.reference})`);
     }
     if (item.fillingRules.options.quantity) {
-      validators.set(item.fillingRules.options.quantity.validatorType,
-        item.fillingRules.options.quantity.data.reference);
+      validators.push(`${item.fillingRules.options.quantity.validatorType}(${item.fillingRules.options.quantity.data.reference})`);
     }
     if (item.fillingRules.options.maxTime) {
-      validators.set(item.fillingRules.options.maxTime.validatorType,
-        item.fillingRules.options.maxTime.data.reference.value);
+      validators.push(`${item.fillingRules.options.maxTime.validatorType}(${item.fillingRules.options.maxTime.data.reference.value})`);
+      if (item.fillingRules.options.minTime) {
+        validators.push(`${item.fillingRules.options.minTime.validatorType}(${item.fillingRules.options.minTime.data.reference.value})`);
+      }
     }
-    if (item.fillingRules.options.minTime) {
-      validators.set(item.fillingRules.options.minTime.validatorType,
-        item.fillingRules.options.minTime.data.reference.value);
-    }
+      return validators;
 
-    return validators;
+
+      // let validators = new Map();
+      //
+      // if (item.fillingRules.options.mandatory) {
+      //   validators.set(item.fillingRules.options.mandatory.validatorType,
+      //     item.fillingRules.options.mandatory.data.reference);
+      // }
+      // if (item.fillingRules.options.accept) {
+      //   validators.set(item.fillingRules.options.accept.validatorType,
+      //     item.fillingRules.options.accept.data.reference);
+      // }
+      // if (item.fillingRules.options.distinct) {
+      //   validators.set(item.fillingRules.options.distinct.validatorType,
+      //     item.fillingRules.options.distinct.data.reference);
+      // }
+      // if (item.fillingRules.options.lowerLimit) {
+      //   validators.set(item.fillingRules.options.lowerLimit.validatorType,
+      //     item.fillingRules.options.lowerLimit.data.reference);
+      // }
+      // if (item.fillingRules.options.upperLimit) {
+      //   validators.set(item.fillingRules.options.upperLimit.validatorType,
+      //     item.fillingRules.options.upperLimit.data.reference);
+      // }
+      // if (item.fillingRules.options.precision) {
+      //   validators.set(item.fillingRules.options.precision.validatorType,
+      //     item.fillingRules.options.precision.data.reference);
+      // }
+      // if (item.fillingRules.options.scale) {
+      //   validators.set(item.fillingRules.options.scale.validatorType,
+      //     item.fillingRules.options.scale.data.reference);
+      // }
+      // if (item.fillingRules.options.in) {
+      //   validators.set(item.fillingRules.options.in.validatorType,
+      //     [item.fillingRules.options.in.data.reference.initial + '~' +
+      //     item.fillingRules.options.in.data.reference.end]);
+      // }
+      // if (item.fillingRules.options.minDate) {
+      //   validators.set(item.fillingRules.options.minDate.validatorType,
+      //     item.fillingRules.options.minDate.data.reference.value,
+      //   );
+      // }
+      // if (item.fillingRules.options.maxDate) {
+      //   validators.set(item.fillingRules.options.maxDate.validatorType,
+      //     item.fillingRules.options.maxDate.data.reference.value,
+      //   );
+      // }
+      // if (item.fillingRules.options.futureDate) {
+      //   validators.set(item.fillingRules.options.futureDate.validatorType,
+      //     item.fillingRules.options.futureDate.data.reference,
+      //   );
+      // }
+      // if (item.fillingRules.options.pastDate) {
+      //   validators.set(item.fillingRules.options.pastDate.validatorType,
+      //     item.fillingRules.options.pastDate.data.reference,
+      //   );
+      // }
+      // if (item.fillingRules.options.rangeDate) {
+      //   validators.set(item.fillingRules.options.rangeDate.validatorType,
+      //     [item.fillingRules.options.rangeDate.data.reference.initial.value + '~' +
+      //     item.fillingRules.options.rangeDate.data.reference.end.value]
+      //   );
+      // }
+      // if (item.fillingRules.options.minSelected) {
+      //   validators.set(item.fillingRules.options.minSelected.validatorType,
+      //     item.fillingRules.options.minSelected.data.reference);
+      // }
+      // if (item.fillingRules.options.maxSelected) {
+      //   validators.set(item.fillingRules.options.maxSelected.validatorType,
+      //     item.fillingRules.options.maxSelected.data.reference);
+      // }
+      // if (item.fillingRules.options.quantity) {
+      //   validators.set(item.fillingRules.options.quantity.validatorType,
+      //     item.fillingRules.options.quantity.data.reference);
+      // }
+      // if (item.fillingRules.options.maxTime) {
+      //   validators.set(item.fillingRules.options.maxTime.validatorType,
+      //     item.fillingRules.options.maxTime.data.reference.value);
+      // }
+      // if (item.fillingRules.options.minTime) {
+      //   validators.set(item.fillingRules.options.minTime.validatorType,
+      //     item.fillingRules.options.minTime.data.reference.value);
+      // }
+      //
+      // return validators;
+
+
   };
-
-
 }());
