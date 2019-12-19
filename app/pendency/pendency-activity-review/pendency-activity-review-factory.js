69c0dbd693569c2171092c410552bc7adddeecc2
Fabiano Ramires <fdrtec@gmail.com>
2019-12-19 08:51:45 -0300

    OMJ-67 refatoração de nomes e conceitos acertados parte 1

--- app/pendency/pendency-activity-review/pendency-activity-review-factory.js
+++ app/pendency/pendency-activity-review/pendency-activity-review-factory.js
@@ -1,35 +1,68 @@
-(function (){
+(function () {
   'use restrict';
 
   angular
     .module('otusjs.model.pendency')
-    .factory('otusjs.model.pendency.PendencyActivityReview', Factory);
+    .factory('otusjs.model.pendency.UserActivityPendency', Factory);
 
   Factory.$inject = [];
 
   function Factory() {
 
-    let self = this;
+    const self = this;
 
     /* Public methods */
     self.create = create;
+    self.fromJsonObject = fromJsonObject;
 
-    function create(){
-      let pendency = new PendencyActivityReview();
-      return pendency;
+    function create(acronym, recruitmentNumber, requester, receiver) {
+      return new UserActivityPendency(acronym, recruitmentNumber, requester, receiver);
+    }
+
+    function fromJsonObject(jsonObject) {
+      //Destruct ES6
+      const {acronym, recruitmentNumber, requester, receiver, id} = JSON.parse(jsonObject);
+      return new UserActivityPendency(acronym, recruitmentNumber, requester, receiver, id);
     }
 
     return self;
   }
 
-  function PendencyActivityReview(){
+  function UserActivityPendency(acronym, recruitmentNumber, requester, receiver, id) {
     const self = this;
-    self.objectType = 'PendencyActivityReview';
-    self.creationDate = ''
-    self.acronym = '';
-    self.recruitmentNumber = '';
-    self.fromUser = '';
-    self.toUser = '';
+    let _id = id || null;
+
+    self.objectType = 'userActivityPendency';
+    self.creationDate = new Date();
+    self.acronym = acronym;
+    self.recruitmentNumber = recruitmentNumber;
+    self.requester = requester;
+    self.receiver = receiver;
+    // self.dueDate = dueDate;
+
+    /* Public Get methods */
+    self.getID = () => _id;
+    self.getCreationDate = () => self.creationDate;
+    self.getAcronym = () => self.acronym;
+    self.getRecruitmentNumber = () => self.recruitmentNumber;
+    self.getRequester = () => self.requester;
+    self.getReceiver = () => self.receiver;
+
+    /* Public  methods */
+    self.toJSON = toJSON;
+
+    function toJSON() {
+      let json = {
+        objectType: self.objectType,
+        creationDate: self.creationDate,
+        acronym: self.acronym,
+        recruitmentNumber: self.recruitmentNumber,
+        requester: self.requester,
+        receiver: self.receiver
+      };
+
+      return JSON.stringify(json);
+    }
   }
 
 
