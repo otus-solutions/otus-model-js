c0a4e2b04f87316d06174165fc75816453f7ea0e
Diogo Ferreira <diogo.rosas.ferreira@gmail.com>
2019-12-27 18:01:19 -0300

    Revert "OMJ-67 refatoração de nomes e conceitos acertados parte 1"
    This reverts commit 69c0dbd693569c2171092c410552bc7adddeecc2.

--- tests/unit/pendency/pendency-activity-review/pendency-activity-review-factory-spec.js
+++ tests/unit/pendency/pendency-activity-review/pendency-activity-review-factory-spec.js
@@ -1,64 +1,25 @@
-(function () {
-  'use restrict';
+describe('PendencyActivityReviewFactory_UnitTest_Suite', () => {
+  let factory;
+  let Injections = [];
+  let Mock = {};
 
-  describe('PendencyActivityReviewFactory_UnitTest_Suite', () => {
-    let factory;
-    let Injections = [];
-    let Mock = {};
-
-    beforeEach(() => {
-      angular.mock.module('otusjs.model.pendency');
-      angular.mock.inject($injector => {
-        //Injections. = $injector.get(' ');
-        factory = $injector.get('otusjs.model.pendency.UserActivityPendency');
-        _prepareMockParameters();
-        Mock.json = JSON.stringify(Mock.userActivityPendencyParameters);
-
-
-      });
+  beforeEach(() => {
+    angular.mock.module('otusjs.model.pendency');
+    angular.mock.inject($injector => {
+      //Injections. = $injector.get(' ');
+      factory = $injector.get('otusjs.model.pendency.PendencyActivityReview');
     });
-
-    it('serviceExistence_check', () => {
-      expect(factory).toBeDefined();
-    });
-
-    it('serviceMethodsExistence_check', () => {
-      expect(factory.create).toBeDefined();
-      expect(factory.fromJsonObject).toBeDefined();
-    });
-
-    it('createMethod_should_return_instanceOf_userActivityPendency', () => {
-      //Destruct ES6
-      const {acronym, recruitmentNumber, receiver, requester, id} = Mock.userActivityPendencyParameters;
-      let expectPendency = factory.create(acronym, recruitmentNumber, receiver, requester, id);
-      expect(expectPendency.objectType).toBe('userActivityPendency');
-    });
-
-    it('fromJsonObjectMethod_should_fit_attributes_of_json_and_return_userActivityPendency', () => {
-      let expectPendency = factory.fromJsonObject(Mock.json);
-      console.log(expectPendency.getID());
-    });
-
-    it('toJsonMethod_should ', function () {
-      //Destruct ES6
-      const {acronym, recruitmentNumber, receiver, requester, id} = Mock.userActivityPendencyParameters;
-      let expectPendency = factory.create(acronym, recruitmentNumber, receiver, requester, id);
-
-      console.log(expectPendency.toJSON());
-    });
-
-
-    function _prepareMockParameters() {
-      Mock.userActivityPendencyParameters = {
-        id: "001",
-        acronym: "ABCD",
-        recruitmentNumber: "12345",
-        requester: "Supervisor",
-        receiver: "Revisor"
-      }
-    }
   });
 
-})();
+  it('serviceExistence_check', () => {
+    expect(factory).toBeDefined();
+  });
 
+  it('serviceMethodsExistence_check', () => {
+    expect(factory.create).toBeDefined();
+  });
 
+  it('createMethod_should_return_ ', () => {
+    console.log(factory.create())
+  });
+});
