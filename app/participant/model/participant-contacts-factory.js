a0e96ca230b75d95dd61638908f5f12943f7b76c
leogut <leogut@gmail.com>
2020-03-11 13:42:36 -0300

    OMJ-68 Construindo métodos para limitar e validar e-mails secundários.

--- app/participant/model/participant-contacts-factory.js
+++ app/participant/model/participant-contacts-factory.js
@@ -1,103 +1,90 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    angular
-      .module('otusjs.model.participant')
-      .factory('otusjs.model.participantContacts.ParticipantContactsFactory', Factory);
+  angular
+    .module('otusjs.model.participant')
+    .factory('otusjs.model.participantContacts.ParticipantContactsFactory', Factory);
 
-    function Factory() {
-      var self = this;
+  function Factory() {
+    var self = this;
 
-      /* Public methods */
-      self.create = create;
+    /* Public methods */
+    self.create = create;
 
-
-      function create(participant, participantContacts) {
-        return new ParticipantContacts(participant, participantContacts);
-      }
-
-      // function fromJson(jsonObject) { let contacts = new ParticipantContacts(jsonObject); }
-
-      return self;
+    function create(participant, participantContacts) {
+      return new ParticipantContacts(participant, participantContacts);
     }
 
-    function ParticipantContacts(participant, participantContacts) {
-      var self = this;
+    // function fromJson(jsonObject) { let contacts = new ParticipantContacts(jsonObject); }
 
-      self.objectType = 'ParticipantContacts';
-      self.recruitNumber = participant.recruitmentNumber || "";
-
-      self.mainEmail = participant.email || ""; //assumir que o e-mail sempre existirá no participant.email (adicionar erro se não houver??)
-
-      if (participant.email) {
-        self.emailsList = [participant.email + ' (principal)']; //[participant.email];
-      } else {
-        self.emailsList = [];
-      }
-
-      if (participantContacts.secondaryEmails) {
-        self.emailsList = self.emailsList.concat(participantContacts.secondaryEmails);
-
-      }
-
-
-      self.phoneNumber = participantContacts.mainPhoneNumber || '';
-      self.secondaryPhoneNumbers = participantContacts.secondaryPhoneNumbers || [];
-      self.mainAddress = participantContacts.mainAddress || {};
-      self.secondaryAddresses = participantContacts.secondaryAddresses || [];
-      // self.secondaryAddresses = participantContacts.secondaryAddresses || [ // Colocar Objeto no formato ou simplesmente deixar o array vazio?
-      //   {
-      //     postalCode: '',
-      //     street: '',
-      //     streetNumber: 0,
-      //     complements: '',
-      //     neighbourhood: '',
-      //     city: '',
-      //     country: '',
-      //     observations: ''
-      //   }
-      //   ];
-
-      // console.log(self.emailsList);
-
-      // self.campo = info.campo || "";
-      // self.emailsList = info.emailsList || [];
-      // self.addEmail = addEmail;
-
-
-      // function addEmail(email) {
-      //   if (self.emailsList.length === 5) {
-      //     self.emailsList.pop();
-      //     // or
-      //     // throw new Error("");
-      //   }
-      //   self.emailsList.push();
-      // }
-      self.toJson = toJson;
-
-      function toJson() {
-        var json = {};
-
-        json.phoneNumber = self.phoneNumber;
-        json.email = self.email;
-        json.address = self.address;
-        return JSON.stringify(json);
-      }
-    }
+    return self;
   }
 
-  ()
-);
+  function ParticipantContacts(participant, participantContacts) {
+    var self = this;
+    var limitErrMsg = "Limite máximo atingido, por favor remova um campo para adicionar um novo contato."
 
+    self.showError = showError;
 
-// function addUser(email) {
-//       if (!_validEmail(email)){
-//         self.showError('Email is invalid!');
-//       } else
-//           if(!self.isUserExists(email)){
-//             self.exclusiveDisjunction.push(email.toLowerCase())
-//           }
-//     }
+    self.objectType = 'ParticipantContacts';
+    self.recruitNumber = participant.recruitmentNumber || "";
+    self.mainEmail = participant.email || "";
+    self.secondaryEmails = participantContacts.secondaryEmails;
+    self.phoneNumber = participantContacts.mainPhoneNumber || '';
+    self.secondaryPhoneNumbers = participantContacts.secondaryPhoneNumbers || [];
+    self.mainAddress = participantContacts.mainAddress || {};
+    self.secondaryAddresses = participantContacts.secondaryAddresses || [{
+      postalCode: '',
+      street: '',
+      streetNumber: '',
+      complements: '',
+      neighbourhood: '',
+      city: '',
+      country: '',
+      observations: ''
+    }];
+
+    /* Public methods */
+    self.toJson = toJson;
+    self.addSecondaryEmail = addSecondaryEmails;
+
+    function showError(msg) {
+      throw new Error(msg);
+    }
+
+    function _validEmail(email) {
+      var Regex = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}/gi;
+      return Regex.test(email);
+    }
+
+    //ToDo: Validar este método e replicar para secondaryPhoneNumbers & secondaryAddresses.
+    function addSecondaryEmails(newEmail) {
+      if (self.secondaryEmails.length === 5) {
+        self.showError(limitErrMsg);
+      } else if (!_validEmail(newEmail)) {
+        self.showError('Email is invalid!');
+      } else {
+        self.secondaryEmails.push(newEmail.toLowerCase())
+      }
+    }
+
+    function toJson() {
+      var json = {};
+      json.objectType = self.objectType;
+      json.recruitmentNumber = self.recruitmentNumber;
+      json.mainEmail = self.mainEmail;
+      json.secondaryEmails = self.secondaryEmails;
+      json.phoneNumber = self.phoneNumber;
+      json.secondaryPhoneNumbers = self.secondaryPhoneNumbers;
+      json.address = self.address;
+      json.secondaryAddresses = self.secondaryAddresses;
+      return JSON.stringify(json);
+    }
+
+    return self;
+  }
+
+}());
 
 
 // Coisas no OTUS:
@@ -106,24 +93,3 @@
 // contact.campo = "";
 // contact.emailsList.push("email@email");
 // contact.addEmail("email@email");
-
-//===================================================================================
-// TODO busca automática dos campos, baseada no CEP.
-//      "CEP": "90010-907", // verificar se há preocupação com o formato (com hífen?; apenas numérico?)
-//
-//   socialMediaList = [
-//   {
-// TODO: adicionar uma flag para salientar se é a rede social principal?
-
-//   redeSocial: 'facebook',
-//   contato: 'mybuddy@fakemail.com'
-//   observacoes: 'Face do meu colega de apartamento, o nome dele é João da Silva.'
-//   },
-//   {
-//   redeSocial: 'whatsapp',
-//   contato: '+55 51 99999-9999'
-//   observacoes: 'É o meu zap-zap pessoal, sempre confiro antes de dormir!'
-//   }
-//   ],
-// }
-//===================================================================================
--- tests/unit/participant/model/participant-contacts-factory-spec.js
+++ tests/unit/participant/model/participant-contacts-factory-spec.js
@@ -30,39 +30,7 @@ fdescribe("ParticipantContactsFactory", function () {
       expect(participantContactsData.objectType).toEqual('ParticipantContacts');
     })
 
-// TODO: dependendo de como ficar a distribuição das listas, com, ou sem 'principais", fazer um  teste
-//  comparando com o mock do "participantContacts"
-
   });
-
-  // it('unitTest: should success response with body parameter', function () {
-  //   let body = { teste: null };
-  //   let valueCustom = responseApp.success(body);
-  //   expect(valueCustom.code).toBe(200);
-  //   expect(valueCustom.body.data).toBe(body);
-  // });
-
-  // it('unitTest: should success response with no body parameter', function () {
-  //   let valueDefault = responseApp.success();
-  //   expect(valueDefault.code).toBe(200);
-  //   expect(valueDefault.body.data).toBe(true);
-  // });
-
-  // it("should return an object named phoneNumber with attribute value equal to +55 011-1406", function () {
-  //   expect(participantContactsData.phoneNumber).toEqual(Mock.participantContacts.phoneNumber);
-  //   console.log(participantContactsData.phoneNumber);
-  //   console.log(Mock.participantContacts.phoneNumber);
-  //   console.log();
-  // })
-
-  // it("should return an object named email with attribute value equal to 'EMAIL'", function () {
-  //   expect(participantContactsData.email).toEqual(EMAIL);
-  //   console.log(participantContactsData.email);
-  //   console.log(Mock.participantContacts.email);
-  //   console.log();
-  // })
-
-// });
 
 
   /*=================================================*/
@@ -88,7 +56,7 @@ fdescribe("ParticipantContactsFactory", function () {
       mainAddress: {
         postalCode: "90010-907",
         street: 'Rua Um',
-        streetNumber: 2,                               // seria melhor string, aqui? ou ainda: seria melhor juntar rua e número no mesmo campo?
+        streetNumber: '2',
         complements: 'Ap. 3',
         neighbourhood: 'Bairro Quatro',
         city: 'Cidade Cinco',
@@ -120,17 +88,50 @@ fdescribe("ParticipantContactsFactory", function () {
     };
 
   };
-  /*============================================================================*/
-
-// var participantContacts = {
-//   objectType: 'ParticipantContacts',
-//   phoneNumber: '+55 011-1406',
-//   email: 'email@mail.com',
-//   secondaryAdresses: []
-// };
-// var participant = {
-//   objectType: 'Participant',
-//   email: 'email@mail.com'
-// };
 
 });
+
+//===================================================================================
+// TODO busca automática dos campos, baseada no CEP.
+//      "CEP": "90010-907", // verificar se há preocupação com o formato (com hífen?; apenas numérico?)
+//
+//   socialMediaList = [
+//   {
+// TODO: adicionar uma flag para salientar se é a rede social principal?
+
+//   redeSocial: 'facebook',
+//   contato: 'mybuddy@fakemail.com'
+//   observacoes: 'Face do meu colega de apartamento, o nome dele é João da Silva.'
+//   },
+//   {
+//   redeSocial: 'whatsapp',
+//   contato: '+55 51 99999-9999'
+//   observacoes: 'É o meu zap-zap pessoal, sempre confiro antes de dormir!'
+//   }
+//   ],
+// }
+//===================================================================================
+
+
+// //Exemplos de testes de outros projetos
+// it('unitTest: should success response with body parameter', function () {
+//   let body = { teste: null };
+//   let valueCustom = responseApp.success(body);
+//   expect(valueCustom.code).toBe(200);
+//   expect(valueCustom.body.data).toBe(body);
+// });
+
+
+// it("should return an object named phoneNumber with attribute value equal to +55 011-1406", function () {
+//   expect(participantContactsData.phoneNumber).toEqual(Mock.participantContacts.phoneNumber);
+//   console.log(participantContactsData.phoneNumber);
+//   console.log(Mock.participantContacts.phoneNumber);
+//   console.log();
+// })
+
+// it("should return an object named email with attribute value equal to 'EMAIL'", function () {
+//   expect(participantContactsData.email).toEqual(EMAIL);
+//   console.log(participantContactsData.email);
+//   console.log(Mock.participantContacts.email);
+//   console.log();
+// })
